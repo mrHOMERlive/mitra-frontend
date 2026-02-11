6e12474bf526f093ba2aea1f329cc0ba1ea3df95
mrHOMERlive <mrHOMERlive@users.noreply.github.com>
2026-02-11 14:56:34 +0300

    ico

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function App() {
 
   return (
     <QueryClientProvider client={queryClientInstance}>
-      <Router>
+      <Router future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
         <Routes>
           <Route path="/" element={
             <LayoutWrapper currentPageName={mainPageKey}>
